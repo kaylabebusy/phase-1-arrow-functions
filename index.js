54d86d9eafed8a681c19309c079c3c9cbb4736d2
Kayla Copeland <kaylabebusy@gmail.com>
2023-05-06 10:06:02 -0400

    lab complete

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (parameter2000, parameter100) => parameter2000 / parameter100
+divide(2000,100);
+
+const square = (parameter2) => parameter2 * parameter2
+square(2)
+
+const add = (parameter4, parameter6) => parameter4 + parameter6
+add(4,6)
